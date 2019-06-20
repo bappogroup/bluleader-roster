be122399c5827d2f82c608a988c60b7564e15c75
Stan <stanley.luo@bappo.com>
2019-06-20 14:29:49 +1000

    Assign project in quick-roster

--- consultant-rosters/index.js
+++ consultant-rosters/index.js
@@ -5,7 +5,8 @@ import {
   styled,
   TouchableView,
   ScrollView,
-  TextInput
+  TextInput,
+  Dropdown
 } from "bappo-components";
 
 class Consultants extends React.Component {
@@ -54,14 +55,47 @@ class Consultants extends React.Component {
     });
   };
 
+  handleCreateAssignment = async assignment => {
+    const { $models } = this.props;
+    const existingAssignment = await $models.ProjectAssignment.findOne({
+      where: {
+        consultant_id: assignment.consultant_id,
+        project_id: assignment.project_id
+      }
+    });
+
+    // Avoid duplication and create new assignment
+    if (!existingAssignment) {
+      await $models.ProjectAssignment.create(assignment);
+    }
+  };
+
   render() {
     return (
       <Container>
-        <SearchInput
-          placeholder="Search consultants"
-          onValueChange={this.onSearchTermChange}
-          value={this.state.searchString}
-        />
+        <Header>
+          <SearchInput
+            placeholder="Search consultants"
+            onValueChange={this.onSearchTermChange}
+            value={this.state.searchString}
+          />
+          <Dropdown
+            icon="menu"
+            actions={[
+              {
+                icon: "assignment-ind",
+                label: "Assign a Project to a Consultant",
+                onPress: () =>
+                  this.props.$popup.form({
+                    title: "New Assignment",
+                    formKey: "ProjectAssignmentForm",
+                    onSubmit: this.handleCreateAssignment
+                  })
+              }
+            ]}
+          />
+        </Header>
+
         <ScrollView>
           {this.state.shortlist.map(c => (
             <Row onPress={() => this.showRoster(c)} key={c.id}>
@@ -76,23 +110,30 @@ class Consultants extends React.Component {
 
 export default Consultants;
 
+const Container = styled(View)`
+  display: flex;
+  flex: 1;
+`;
+
+const Header = styled(View)`
+  flex-direction: row;
+  margin: 16px;
+  align-items: center;
+`;
+
 const Row = styled(TouchableView)`
   height: 40px;
   justify-content: center;
   align-items: center;
 `;
 
-const Container = styled(View)`
-  display: flex;
-  flex: 1;
-`;
-
 const SearchInput = styled(TextInput)`
+  flex: 1;
+  height: 40px;
   border-style: solid;
   border-width: 2px;
   border-color: #ddd;
   border-radius: 3px;
-  margin: 20px;
-  height: 40px;
-  padding-left: 20px;
+  margin-right: 16px;
+  padding-left: 16px;
 `;
